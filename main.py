2e7cbfdef16eb254a51a675125502c87514c1a6c
ApacheWarlord <dhimanpulkit38496@gmail.com>
2025-07-25 23:39:52 +0200

    Added group and containers

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from circleshape import *
 def main():
     print("Starting Asteroids!")
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers =(updatable,drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Astroids Game")
     clock = pygame.time.Clock()
@@ -19,8 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)
         
