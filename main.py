8f440a80d6a8f0fef30b94d3a6c195be0da0eb5b
ApacheWarlord <dhimanpulkit38496@gmail.com>
2025-07-25 21:03:24 +0200

    changes to code

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from constants import *
 import pygame
+from constants import *
+
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:",SCREEN_WIDTH)
-    print("Screen height:",SCREEN_HEIGHT)
+    pygame.init()  # <-- Very important!
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Astroids Game")
+    clock = pygame.time.Clock()
+    running = True
 
-
-if __name__ == "__main__":
-    main()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
